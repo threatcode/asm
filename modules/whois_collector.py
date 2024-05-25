3af6f08879be6d10186faf94f4066fd5c6fbe9e5
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-05-25 13:00:48 +0600

    Create whois_collector.py

---
+++ modules/whois_collector.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#   Filename: whois_collector.py
+#   Module: Whois Collector
+
+# External Libraries
+from ipwhois import IPWhois
+
+
+def wlookup(hostx):
+    for ip in hostx.resolved_ips:
+        try:
+            ip_object = IPWhois(ip.address)
+            query = ip_object.lookup_rdap(depth=1)
+            #            hostx.whois.append(query)
+            #            net_sec = query.get('network', {})
+            ip.location = query.get('asn_country_code')
+            ip.asn = query.get('asn')
+            ip.cidr = query.get('asn_cidr')
+        #            print("Executed")
+        #            print(query['network']['name'])
+        except:
+            pass
